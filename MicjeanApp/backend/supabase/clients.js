b794ad9669aec8e7c182c13a3f4271191f0822f2
Niiaks <napappoe007@st.ug.edu.gh>
2025-06-26 17:20:13 +0000

    removed hardcoded credentials and imporoved appstate listener to prevent memory leaks

--- MicjeanApp/backend/supabase/clients.js
+++ MicjeanApp/backend/supabase/clients.js
@@ -1,10 +1,10 @@
-import { AppState } from 'react-native'
-import 'react-native-url-polyfill/auto'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient, processLock } from '@supabase/supabase-js'
+import { AppState } from "react-native";
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient, processLock } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_PROJECT_URL || 'https://pxecpqenfddoprilkjqw.supabase.co'
-const supabaseAnonKey = process.env.EXPO_PUBLIC_API_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4ZWNwcWVuZmRkb3ByaWxranF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3OTcyMzMsImV4cCI6MjA2NjM3MzIzM30.Y5EODVzv3m6UBTQBLnGJk1gC3h0f2PBrvYM4RlNyYXs'
+const supabaseUrl = process.env.EXPO_PUBLIC_PROJECT_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_API_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
@@ -14,12 +14,25 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
         detectSessionInUrl: false,
         lock: processLock,
     },
-})
+});
 
-AppState.addEventListener('change', (state) => {
-    if (state === 'active') {
-        supabase.auth.startAutoRefresh()
+AppState.addEventListener("change", (state) => {
+    if (state === "active") {
+        supabase.auth.startAutoRefresh();
     } else {
-        supabase.auth.stopAutoRefresh()
+        supabase.auth.stopAutoRefresh();
     }
-})
+});
+let appStateListener;
+
+if (appStateListener) {
+    appStateListener.remove();
+}
+
+appStateListener = AppState.addEventListener("change", (state) => {
+    if (state === "active") {
+        supabase.auth.startAutoRefresh();
+    } else {
+        supabase.auth.stopAutoRefresh();
+    }
+});
