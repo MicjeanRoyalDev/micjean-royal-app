13f1ff30f25780f7c7150baa10266f78fa1bb299
Isaac <nisaacdz@gmail.com>
2025-07-04 05:32:29 +0000

    Add useDebounce hook

---
+++ MicjeanApp/apps/Vendor/hooks/useDebounce.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+export function useDebounceNumber(value: number, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
